dd4952903709435582fff20654c7a919ccb216f6
Filip Tanurovski <filiptanu@gmail.com>
2022-05-27 16:35:10 +0200

    Refactor views to support different formats

--- qogita_api/addresses/urls.py
+++ qogita_api/addresses/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from addresses import views
 
 urlpatterns = [
     path('addresses/', views.address_list),
     path('addresses/<uuid:address_uuid>/', views.address_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- qogita_api/addresses/views.py
+++ qogita_api/addresses/views.py
@@ -1,45 +1,43 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from addresses.models import Address
 from addresses.serializers import AddressSerializer
 
 
-@csrf_exempt
-def address_list(request):
+@api_view(['GET', 'POST'])
+def address_list(request, format=None):
     if request.method == 'GET':
         addresses = Address.objects.all()
         serializer = AddressSerializer(addresses, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = AddressSerializer(data=data)
+        serializer = AddressSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def address_detail(request, address_uuid):
+@api_view(['GET', 'PUT', 'DELETE'])
+def address_detail(request, address_uuid, format=None):
     try:
         address = Address.objects.get(uuid=address_uuid)
     except Address.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = AddressSerializer(address)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = AddressSerializer(address, data=data)
+        serializer = AddressSerializer(address, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         address.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
